6138380b4d53ef1c50cffd4684b28784fe6007c5
zhihao <zhihao@softchef.com>
2019-09-27 17:23:19 +0800

    update

--- controller/linux/index.js
+++ controller/linux/index.js
@@ -1,28 +1,31 @@
 const io = require('socket.io-client')
 const ip = process.argv[2]
 const { execSync } = require('child_process')
-  
-let socket = io(ip, { query:'type=controller' })
+
+let socket = io(ip, { query: 'type=controller' })
+let working = false
+
+function work(command) {
+    if (working === true) {
+        return null
+    }
+    working = true
+    execSync(command)
+    working = false
+}
 
 socket.on('GetMousePos', () => {
     socket.emit('MousePos', `[${5}, ${10}]`)
 })
 
 socket.on('Controller', (data) => {
-    if (data.type === 'KeyDown') {
-        exec(`xdotool key ${data.value}`)
-        return null
-    }
     if (data.type === 'MouseMove') {
-	console.log('move', data)
-	let prefix = data.value[0] < 0 ? '--' : ''
-try {
-        
-} catch(e) { console.log(e) }
+        let prefix = data.value[0] < 0 ? '--' : ''
+        work(`xdotool mousemove_relative ${prefix} ${data.value[0]} ${data.value[1]}`)
         return null
     }
 })
 
 setInterval(() => {
-	console.log('ccc')
+    console.log('ccc')
 }, 1000)
